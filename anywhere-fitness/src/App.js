c087def1e8f702ca8fb572821876696542924827
unknown <teresafranxman97@gmail.com>
2021-05-24 16:48:09 -0500

    Added Routing

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import InstructorDash from './Components/Instructor/InstructorDashboard';
+import ClientDashboard from './Components/Client/ClientDashboard';
 
 function App() {
   return (
     <div className="App">
       <header>Anywhere Fitness</header>
+
+      <Switch>
+        <Route path='/dashboard' >
+          <InstructorDash />
+        </Route>
+        <Route path='/clienthome'>
+          <ClientDashboard />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ anywhere-fitness/src/Components/Instructor/InstructorDashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InstructorDash = () => {
+    return (
+        <h1>Welcome back!</h1>
+    )
+}
+
+export default InstructorDash;
