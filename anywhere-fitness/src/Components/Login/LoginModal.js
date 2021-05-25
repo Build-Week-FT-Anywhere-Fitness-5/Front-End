3ed1c1d01d802d523b45e9c56c40bb97038f6217
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-05-25 16:00:53 -0400

    toggle instructor authorization in login form

--- anywhere-fitness/src/Components/Login/LoginModal.js
+++ anywhere-fitness/src/Components/Login/LoginModal.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginModal = () => {
+    const [instructorChecked, setInstructorChecked] = useState(false);
+    const handleInstructorCheck = e => {
+        setInstructorChecked(!instructorChecked);
+    }
+
     return (
         <div>
-            Login Modal
+            <form>
+                <label>
+                    <input type="checkbox" name="" id="" checked={instructorChecked} onChange={e => handleInstructorCheck(e)}/>
+                    Check if you are an instructor
+                </label><br />
+                <label>
+                    User Name
+                    <input type="text" name="" id="" placeholder="Type in your user name"/>
+                </label><br />
+                <label>
+                    Password
+                    <input type="password" name="" id="" placeholder="Type in your user name"/>
+                </label>
+                <br />
+                {instructorChecked && <label>
+                    Instructor Authorization
+                    <input type="password" name="" id="" placeholder="Type in your user name"/>
+                </label>}
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
