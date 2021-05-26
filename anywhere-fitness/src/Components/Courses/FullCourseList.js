8ec4294936fa7eb0d1d1ef6d9b15b33d6091b6ad
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-05-26 15:14:49 -0400

    course components

---
+++ anywhere-fitness/src/Components/Courses/FullCourseList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FullCourseList = () => {
+    return(
+        <div>FullCourseList</div>
+    )
+}
+
+export default FullCourseList;
--- anywhere-fitness/src/Components/Header/InstructorProfileNav.js
+++ anywhere-fitness/src/Components/Header/InstructorProfileNav.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import OfferedCourses from '../Courses/OfferedCourses';
+import EnrolledCourses from '../Courses/EnrolledCourses';
+import FullCourseList from '../Courses/FullCourseList';
 
 const InstructorProfileNav = props => {
     const { loginData } = props;
+
+    const [offered, setOffered] = useState(true);
+    const [enrolled, setEnrolled] = useState(false);
+    const [full, setFull] = useState(false);
+
+    const handleOffered = () => {
+        setOffered(true);
+        setEnrolled(false);
+        setFull(false);
+    }
+    const handleEnrolled = () => {
+        setOffered(false);
+        setEnrolled(true);
+        setFull(false);
+    }
+    const handleFull = () => {
+        setOffered(false);
+        setEnrolled(false);
+        setFull(true);
+    }
 
     const Section = styled.section`
         background-color: pink;
     `;
 
     return (
+        <>
         <Section>
             <div className="instructorProfileImg"></div>
             <h3>{loginData.userName}</h3>
+            <button onCLick={handleOffered}>My Offered Courses</button>
+            <button onCLick={handleEnrolled}>My Enrolled Courses</button>
+            <button onCLick={handleFull}>Full Course Catalog</button>
         </Section>
+        {offered && <OfferedCourses />}
+        {enrolled && <EnrolledCourses />}
+        {full && <FullCourseList />}
+        </>
     )
 }
 
