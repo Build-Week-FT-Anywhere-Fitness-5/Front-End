b5a8e893d1b1c2143d766e99f14e6db6ac0d63d0
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-05-28 10:58:57 -0400

    add global styling

--- anywhere-fitness/src/Components/Header/Nav.js
+++ anywhere-fitness/src/Components/Header/Nav.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { ReactComponent as Logo } from  './af_logo.svg';
 
 const NavStyle = styled.nav`
-    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    padding: 10px 30px;
+    background-color: #b0234d;
+
+    img {
+        height: 50px;
+        filter: invert(10%);
+        opacity: 0.6;
+        transition: all 0.5s ease-in-out;
+
+        &:hover {
+            filter: invert(95%);
+            transition: all 0.3s ease-in-out;
+        }
+    }
 `;
 
 const Nav = props => {
@@ -12,12 +30,6 @@ const Nav = props => {
     const [onHomePage, setOnHomePage] = useState(true);
     const [onProfilePage, setOnProfilePage] = useState(false);
 
-    const handleRouteChange = e => {
-        setOnHomePage(false);
-        setOnProfilePage(true);
-        setLoginModalOpen(false);
-    }
-
     const handleHomeRoute = e => {
         setOnHomePage(true);
         setOnProfilePage(false);
@@ -25,14 +37,9 @@ const Nav = props => {
 
     return (
         <NavStyle>
-            <h2>Nav Bar</h2>
-            <p>import Client and Instructor Search Components and render them conditionaly</p>
+            <Link to="/" onClick={handleHomeRoute}><Logo height="60px" fill="#eaff00"/></Link>
             {onHomePage && <button onClick={openLogin}>Login/ Sign-UP</button>}
             {onProfilePage && <button>Log Out</button>}
-            <br/>
-            <Link to="/clienthome" onClick={handleRouteChange}>Client Temp</Link><br/>
-            <Link to="/dashboard" onClick={handleRouteChange}>Instructor Temp</Link><br/>
-            <Link to="/" onClick={handleHomeRoute}>Home</Link>
         </NavStyle>
     )
 }
